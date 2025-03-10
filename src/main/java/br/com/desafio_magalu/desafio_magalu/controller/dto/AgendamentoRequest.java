9d8dcc770f78ac1ff395d7c227ee9dfdacbcb7b5
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-03-10 17:38:30 -0300

    feat: adiciona DTO para o request de agendamento

---
+++ src/main/java/br/com/desafio_magalu/desafio_magalu/controller/dto/AgendamentoRequest.java
@@ -0,0 +1,21 @@
+package br.com.desafio_magalu.desafio_magalu.controller.dto;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record AgendamentoRequest(
+
+        @NotNull(message = "A data/hora não pode estar em branco")
+        @FutureOrPresent(message = "Não é permitido agendamentos com datas no passado")
+        LocalDateTime dataHora,
+
+        @NotBlank(message = "O destinatário não pdoe estar em branco")
+        String destinatario,
+
+        @NotBlank(message = "A mensagem não pode estar em branco")
+        String mensagem
+) {
+}
